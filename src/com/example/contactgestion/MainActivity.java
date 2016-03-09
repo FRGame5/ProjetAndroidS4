f40dece85f5d614471c33f6658a05e3cf40b18ba
ttt1450a <ttt1450a@r-info-ambr-e09.iut.iut-tlse3.fr>
2016-03-09 15:02:20 +0100

    Ajout seconde activité + lien entre les 2

--- src/com/example/contactgestion/MainActivity.java
+++ src/com/example/contactgestion/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.contactgestion;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void ajouterContact(View v) {
+		if (v instanceof Button) {
+			Button b = (Button) v;
+			switch(b.getId()) {
+			case R.id.button1:
+				Intent intent = new Intent(this,Ajout_ModifContact.class);
+				startActivity(intent);
+				break;
+			}
+		}
 	}
 }
