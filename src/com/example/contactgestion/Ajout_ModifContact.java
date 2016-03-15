c974347c24040466b49490cefbaf07a2d163e92b
ttt1450a <ttt1450a@r-info-perl-e07.iut.iut-tlse3.fr>
2016-03-15 12:29:16 +0100

    azdefgjkvgj

--- src/com/example/contactgestion/Ajout_ModifContact.java
+++ src/com/example/contactgestion/Ajout_ModifContact.java
@@ -1,10 +1,14 @@
 package com.example.contactgestion;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Ajout_ModifContact extends Activity {
 
@@ -14,5 +18,28 @@ public class Ajout_ModifContact extends Activity {
 		setContentView(R.layout.activity_ajout__modif_contact);
 	}
 	
+	public void insertContact(View v)
+	{
+		String surnom = ((EditText)findViewById(R.id.textSurnom)).getText().toString();
+		String telephone = ((EditText)findViewById(R.id.textTelephone)).getText().toString();
+		
+		RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+		
+		String sexe = ((RadioButton)findViewById(rg.getCheckedRadioButtonId())).getText().toString() ;
+		
+		DBQuery db = new DBQuery(this);
+		
+		SQLiteDatabase bdd = db.getWritableDatabase();
+		
+		ContentValues values = new ContentValues();
+		values.put("surnom", surnom);
+		values.put("sexe", sexe);
+		values.put("telephone", telephone);
+		
+		bdd.insert("Contacts", null, values);
+		Toast.makeText(this, "ajout de "+surnom+" effectué", Toast.LENGTH_SHORT).show();
+		bdd.close();
+		this.finish();
+	}
 	
 }
--- src/com/example/contactgestion/MainActivity.java
+++ src/com/example/contactgestion/MainActivity.java
@@ -1,30 +1,49 @@
 package com.example.contactgestion;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	
+	private List<String> liste;
+	private ArrayAdapter<String> adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.updateListView();
 	}
 	
+	
+	/**
+	 * lance l'activité permettant d'ajouter un contact
+	 * @param v
+	 */
 	public void ajouterContact(View v) {
 		if (v instanceof Button) {
 			Button b = (Button) v;
 			switch(b.getId()) {
-			case R.id.button1:
+			case R.id.bt_annuler_ajout_contact:
+				// lance l'activité permettant d'ajouter un contact
 				Intent intent = new Intent(this,Ajout_ModifContact.class);
 				startActivity(intent);
 				break;
 			}
 		}
 	}
+	
+	public void updateListView()
+	{
+		ListView lView = (ListView)findViewById(R.id.listView1);
+	}
+	
+	
 }
